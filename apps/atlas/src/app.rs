d10fe76d9d4d12228270709b6a18be862807c560
J. Zane Cook <90814067+jzanecook@users.noreply.github.com>
2024-01-10 21:38:55 -0600

    fix(atlas): Added chat window connecting to ollama API

--- apps/atlas/src/app.rs
+++ apps/atlas/src/app.rs
@@ -20,6 +20,20 @@ pub struct NapkinSettings {
     service: NapkinService,
 }
 
+#[derive(Clone, Serialize, Deserialize)]
+pub struct ChatHistory {
+    /** Used to handle multiple chats */
+    instance: u32,
+    /** Visual user representation */
+    user: String,
+    /** Message text */
+    message: String,
+    /** Model used, if applicable */
+    model: Option<String>,
+    /** Timestamp of message */
+    timestamp: String,
+}
+
 impl NapkinSettings {
     pub fn default() -> Self {
         Self {
@@ -38,14 +52,42 @@ pub enum Theme {
     Dark,
 }
 
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub struct OllamaRequest {
+    prompt: String,
+    model: String,
+    stream: bool,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub struct OllamaGenerateResponse {
+    model: String,
+    created_at: String,
+    response: String,
+    done: Option<bool>,
+    context: Option<Vec<i64>>,
+    total_duration: Option<i64>,
+    load_duration: Option<i64>,
+    prompt_eval_count: Option<i64>,
+    prompt_eval_duration: Option<i64>,
+    eval_count: Option<i64>,
+    eval_duration: Option<i64>,
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+pub enum AsyncMessage {
+    OllamaGenerateResponse(OllamaGenerateResponse),
+    OllamaStatusCheck(String),
+}
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct AtlasApp {
     #[serde(skip)]
-    tx: Sender<String>,
+    tx: Sender<AsyncMessage>,
     #[serde(skip)]
-    rx: Receiver<String>,
+    rx: Receiver<AsyncMessage>,
     // Example stuff:
     label: String,
     #[serde(skip)] // This how you opt-out of serialization of a field
@@ -53,9 +95,13 @@ pub struct AtlasApp {
     theme: Theme,
     side_panel_open: bool,
     settings_window_open: bool,
+    about_window_open: bool,
     napkin_settings: NapkinSettings,
     napkin_temp_settings: NapkinSettings,
+    #[serde(skip)]
     ollama_check: String,
+    current_prompt: String,
+    chat_history: Vec<ChatHistory>,
 }
 
 impl Default for AtlasApp {
@@ -69,9 +115,12 @@ impl Default for AtlasApp {
             theme: Theme::Dark,
             side_panel_open: true,
             settings_window_open: false,
+            about_window_open: false,
             napkin_settings: NapkinSettings::default(),
             napkin_temp_settings: NapkinSettings::default(),
-            ollama_check: "Not checked".to_owned(),
+            ollama_check: "❌".to_owned(),
+            current_prompt: "".to_owned(),
+            chat_history: vec![],
         }
     }
 }
@@ -98,6 +147,18 @@ impl AtlasApp {
     pub fn revert_settings(&mut self) {
         self.napkin_temp_settings = self.napkin_settings.clone();
     }
+
+    pub fn submit_prompt(&mut self, ctx: &egui::Context) {
+        self.chat_history.push(ChatHistory {
+            instance: 0,
+            user: "USER".to_owned(),
+            message: self.current_prompt.clone(),
+            model: None,
+            timestamp: "NOW".to_owned(),
+        });
+        ollama_send_prompt(self, self.tx.clone(), ctx.clone());
+        self.current_prompt = "".to_owned();
+    }
 }
 
 impl eframe::App for AtlasApp {
@@ -108,8 +169,21 @@ impl eframe::App for AtlasApp {
 
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        if let Ok(status) = self.rx.try_recv() {
-            self.ollama_check = format!("Connection successful: {}", status).to_owned();
+        if let Ok(async_message) = self.rx.try_recv() {
+            match async_message {
+                AsyncMessage::OllamaGenerateResponse(response) => {
+                    self.chat_history.push(ChatHistory {
+                        instance: 0,
+                        user: "OLLAMA".to_owned(),
+                        message: response.response,
+                        model: Some(response.model),
+                        timestamp: response.created_at,
+                    });
+                }
+                AsyncMessage::OllamaStatusCheck(_) => {
+                    self.ollama_check = "✅".to_owned();
+                }
+            }
         }
         set_theme(
             &ctx,
@@ -137,6 +211,9 @@ impl eframe::App for AtlasApp {
                             ctx.send_viewport_cmd(egui::ViewportCommand::Close);
                         }
                     });
+                    if ui.button("About").clicked() {
+                        self.about_window_open = true;
+                    }
                 }
 
                 ui.with_layout(egui::Layout::right_to_left(egui::Align::Max), |ui| {
@@ -168,18 +245,19 @@ impl eframe::App for AtlasApp {
         egui::SidePanel::left("left_panel")
             .resizable(true)
             .show_animated(ctx, self.side_panel_open, |ui| {
-                ui.heading("Side Panel");
+                ui.set_min_width(200.0);
 
-                if ui
-                    .button("Check Connection")
-                    .on_hover_text("Check connection to Ollama server.")
-                    .clicked()
-                {
-                    println!("Checking connection to Ollama server...");
-                    check_ollama(self, self.tx.clone(), ctx.clone());
-                }
-                ui.label(&self.ollama_check);
-                ui.allocate_space(ui.available_size());
+                ui.horizontal(|ui| {
+                    if ui
+                        .button("Check Connection")
+                        .on_hover_text("Check connection to Ollama server.")
+                        .clicked()
+                    {
+                        check_ollama(self, self.tx.clone(), ctx.clone());
+                    }
+                    ui.label(&self.ollama_check);
+                });
+                ui.separator();
             });
 
         egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
@@ -197,10 +275,11 @@ impl eframe::App for AtlasApp {
         });
         central_panel(ctx, self);
         settings_window(ctx, self);
+        about_window(ctx, self);
     }
 }
 
-fn check_ollama(app: &AtlasApp, tx: Sender<String>, ctx: egui::Context) {
+fn check_ollama(app: &AtlasApp, tx: Sender<AsyncMessage>, ctx: egui::Context) {
     let host = app.napkin_settings.service.host.clone();
     let port = app.napkin_settings.service.port.clone();
     tokio::spawn(async move {
@@ -213,39 +292,209 @@ fn check_ollama(app: &AtlasApp, tx: Sender<String>, ctx: egui::Context) {
             .await
             .expect("Unable to parse response");
 
-        let _ = tx.send(body);
+        let _ = tx.send(AsyncMessage::OllamaStatusCheck(body));
         ctx.request_repaint();
     });
 }
 
+fn ollama_send_prompt(app: &AtlasApp, tx: Sender<AsyncMessage>, ctx: egui::Context) {
+    let host = app.napkin_settings.service.host.clone();
+    let port = app.napkin_settings.service.port.clone();
+    let current_prompt = app.current_prompt.clone();
+    let model = app.napkin_settings.model.clone();
+    let request = OllamaRequest {
+        prompt: current_prompt,
+        model,
+        stream: false,
+    };
+    println!("Request: {:?}", request);
+    tokio::spawn(async move {
+        match Client::default()
+            .post(format!("http://{}:{}/api/generate", host, port))
+            .json(&request)
+            .send()
+            .await
+        {
+            Ok(response) => {
+                match response.json().await {
+                    Ok(json) => {
+                        let ollama_gen_response = json;
+                        println!("Response: {:?}", ollama_gen_response);
+                        let _ = tx.send(AsyncMessage::OllamaGenerateResponse(ollama_gen_response));
+                        ctx.request_repaint();
+                    }
+                    Err(e) => {
+                        println!("Unable to parse response: {}", e);
+                    }
+                };
+            }
+            Err(e) => {
+                println!("Unable to send request: {}", e);
+            }
+        };
+    });
+}
+
 fn central_panel(ctx: &egui::Context, app: &mut AtlasApp) {
-    egui::CentralPanel::default()
-    .show(ctx, |ui| {
-    // The central panel the region left after adding TopPanel's and SidePanel's
-    // ui.heading("eframe template");
+    egui::CentralPanel::default().show(ctx, |ui| {
+        // The central panel the region left after adding TopPanel's and SidePanel's
+        // ui.heading("eframe template");
 
-    // ui.horizontal(|ui| {
-    //     ui.label("Write something: ");
-    //     ui.text_edit_singleline(&mut self.label);
-    // });
+        // ui.horizontal(|ui| {
+        //     ui.label("Write something: ");
+        //     ui.text_edit_singleline(&mut self.label);
+        // });
 
-    // ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-    // if ui.button("Increment").clicked() {
-    //     self.value += 1.0;
-    // }
+        // ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
+        // if ui.button("Increment").clicked() {
+        //     self.value += 1.0;
+        // }
 
-    ui.heading("Project: Atlas Napkin");
-    ui.label("So here's the plan:\n\nThe purpose of this application is to serve as the frontend to a locally run AI agent. This agent will do the following:\n\n1. Parse a codebase, or other information.\n2. Map the data into a network graph with vector database.\n3. Use the data in prompting along with multiple other elements to create a cohesive change to codebases.");
+        ui.with_layout(egui::Layout::right_to_left(egui::Align::TOP), |ui| {
+            if ui.button("Configure").clicked() {
+                app.settings_window_open = true;
+            }
+            ui.add_space(8.0);
+            ui.horizontal(|ui| {
+                ui.label(
+                    egui::RichText::new(&app.napkin_settings.model).color(match app.theme {
+                        Theme::Light => LATTE.sapphire,
+                        Theme::Dark => MACCHIATO.sapphire,
+                    }),
+                );
+                ui.label("Model: ");
+            });
+            ui.add_space(8.0);
+            ui.horizontal(|ui| {
+                ui.label(
+                    egui::RichText::new(&app.napkin_settings.service.port).color(match app.theme {
+                        Theme::Light => LATTE.sapphire,
+                        Theme::Dark => MACCHIATO.sapphire,
+                    }),
+                );
+                ui.label("Port: ");
+            });
+            ui.add_space(8.0);
+            ui.horizontal(|ui| {
+                ui.label(
+                    egui::RichText::new(&app.napkin_settings.service.host).color(match app.theme {
+                        Theme::Light => LATTE.sapphire,
+                        Theme::Dark => MACCHIATO.sapphire,
+                    }),
+                );
+                ui.label("Host: ");
+            });
+        });
+        let user_color = match app.theme {
+            Theme::Light => LATTE.flamingo,
+            Theme::Dark => MACCHIATO.flamingo,
+        };
+        let ai_color = match app.theme {
+            Theme::Light => LATTE.sapphire,
+            Theme::Dark => MACCHIATO.sapphire,
+        };
+        egui::Frame::none()
+            .rounding(4.0)
+            .inner_margin(egui::vec2(4.0, 4.0))
+            .stroke(egui::Stroke::new(
+                1.0,
+                match app.theme {
+                    Theme::Light => LATTE.surface1,
+                    Theme::Dark => MACCHIATO.surface1,
+                },
+            ))
+            .show(ui, |ui| {
+                egui::ScrollArea::vertical()
+                    .max_height(400.0)
+                    .show(ui, |ui| {
+                        egui_extras::TableBuilder::new(ui)
+                            .column(egui_extras::Column::auto().resizable(true))
+                            .column(egui_extras::Column::remainder().resizable(true))
+                            .column(egui_extras::Column::auto().resizable(true))
+                            .header(20.0, |mut header| {
+                                header.col(|ui| {
+                                    ui.label(egui::RichText::new("User").color(match app.theme {
+                                        Theme::Light => LATTE.sapphire,
+                                        Theme::Dark => MACCHIATO.sapphire,
+                                    }));
+                                });
+                                header.col(|ui| {
+                                    ui.label(egui::RichText::new("Message").color(
+                                        match app.theme {
+                                            Theme::Light => LATTE.sapphire,
+                                            Theme::Dark => MACCHIATO.sapphire,
+                                        },
+                                    ));
+                                });
+                                header.col(|ui| {
+                                    ui.label(egui::RichText::new("Timestamp").color(
+                                        match app.theme {
+                                            Theme::Light => LATTE.sapphire,
+                                            Theme::Dark => MACCHIATO.sapphire,
+                                        },
+                                    ));
+                                });
+                            })
+                            .body(|mut body| {
+                                for prompt in app.chat_history.iter() {
+                                    body.row(30.0, |mut row| {
+                                        row.col(|ui| {
+                                            ui.label(egui::RichText::new(&prompt.user).color(
+                                                if &prompt.user == "USER" {
+                                                    user_color
+                                                } else {
+                                                    ai_color
+                                                },
+                                            ));
+                                        });
+                                        row.col(|ui| {
+                                            egui::Frame::none()
+                                                .rounding(2.0)
+                                                .inner_margin(egui::vec2(4.0, 4.0))
+                                                .outer_margin(egui::vec2(8.0, 0.0))
+                                                .stroke(egui::Stroke::new(
+                                                    1.0,
+                                                    match app.theme {
+                                                        Theme::Light => LATTE.surface0,
+                                                        Theme::Dark => MACCHIATO.surface0,
+                                                    },
+                                                ))
+                                                .show(ui, |ui| {
+                                                    egui::ScrollArea::vertical()
+                                                        .max_height(400.0)
+                                                        .show(ui, |ui| {
+                                                            ui.label(&prompt.message);
+                                                        });
+                                                });
+                                        });
+                                        row.col(|ui| {
+                                            ui.label(&prompt.timestamp);
+                                        });
+                                    });
+                                }
+                            });
+                    });
+            });
 
-    ui.separator();
+        ui.add_space(4.0);
 
-    ui.label(format!("Host: {}, Port: {}", app.napkin_settings.service.host, app.napkin_settings.service.port));
-
-    // ui.add(egui::github_link_file!(
-    //     "https://github.com/emilk/eframe_template/blob/master/",
-    //     "Source code."
-    // ));
-});
+        ui.with_layout(egui::Layout::top_down(egui::Align::RIGHT), |ui| {
+            ui.horizontal(|ui| {
+                ui.add_space(8.0);
+                if ui.button("Send").clicked() {
+                    app.submit_prompt(&ctx);
+                }
+                let edit_response = ui.add_sized(
+                    ui.available_size(),
+                    egui::TextEdit::singleline(&mut app.current_prompt),
+                );
+                if edit_response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+                    app.submit_prompt(&ctx);
+                    edit_response.request_focus();
+                }
+            });
+        });
+    });
 
     if ctx.input(|i| i.modifiers.ctrl && i.key_pressed(Key::B)) {
         app.side_panel_open = !app.side_panel_open;
@@ -296,5 +545,34 @@ fn settings_window(ctx: &egui::Context, app: &mut AtlasApp) {
         } else {
             app.revert_settings();
         }
+    }
+}
+
+fn about_window(ctx: &egui::Context, app: &mut AtlasApp) {
+    let mut should_close = false;
+    egui::Window::new("About")
+        .id(egui::Id::new("about_window"))
+        .open(&mut app.about_window_open)
+        .resizable(false)
+        .collapsible(false)
+        .title_bar(false)
+        .show(ctx, |ui| {
+            if ui.interact(ui.max_rect(), egui::Id::new("about_window"), egui::Sense::click()).clicked() {
+                should_close = true;
+            }
+            ui.heading("Project: Atlas Napkin");
+            ui.label("So here's the plan:\n\nThe purpose of this application is to serve as the frontend to a locally run AI agent. This agent will do the following:\n\n1. Parse a codebase, or other information.\n2. Map the data into a network graph with vector database.\n3. Use the data in prompting along with multiple other elements to create a cohesive change to codebases.");
+
+            ui.separator();
+            ui.label("This is a project by Z90 Studios.");
+            ui.label("This project is licensed under the MIT license.");
+            ui.horizontal(|ui| {
+                ui.label("This project is open source and can be found on");
+                ui.hyperlink_to(egui::RichText::new("GitHub").color(MACCHIATO.blue), "https://github.com/Z90-Studios/napkin");
+            });
+        });
+
+    if should_close {
+        app.about_window_open = false;
     }
 }
